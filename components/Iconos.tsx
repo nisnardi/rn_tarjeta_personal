93cf740576307da4d9a3ad71e391ccd8f0c9f4ec
Nicolas Isnardi <nicolasisnardi@gmail.com>
2025-03-02 08:13:17 -0500

    * Removed Pressable from Icons.tsx

--- components/Iconos.tsx
+++ components/Iconos.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Pressable } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Icono } from "@/components/Icono";
 
 interface IconosProps {
